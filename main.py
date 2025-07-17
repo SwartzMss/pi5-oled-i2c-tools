5117802b4301e209c3acdbf3ca2de6af05486c60
OverLoad <SwartzMss@users.noreply.github.com>
2025-07-17 23:04:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from oled_display import init_display, show_environment
 
 
 def main():
-    device = init_display(i2c_port=10, i2c_address=0x3C, driver="sh1106")
+    device = init_display(i2c_port=1, i2c_address=0x3C, driver="sh1106")
     # 默认演示温度 25 摄氏度，湿度 40%
     show_environment(device, temperature=25.0, humidity=40.0)
 
